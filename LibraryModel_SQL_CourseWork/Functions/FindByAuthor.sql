49e2cdae739b3e2ff0cacf70b32ad1e3c2498b5e
Andrew Zubkov <99863211+METALROK@users.noreply.github.com>
2025-03-19 19:54:05 +0300

    Create FindByAuthor.sql

---
+++ LibraryModel_SQL_CourseWork/Functions/FindByAuthor.sql
@@ -0,0 +1,32 @@
+CREATE FUNCTION FindByAuthor (@id INT)
+RETURNS @tabl TABLE (
+	PublicationID INT, 
+	Title NVARCHAR(50), 
+	Author NVARCHAR(50),
+	Topic NVARCHAR(50), 
+	Category NVARCHAR(50),
+	PublicationHouse NVARCHAR(50), 
+	PublicationPlace NVARCHAR(50), 
+	ReleaseDate DATE
+) AS
+BEGIN
+	INSERT @tabl
+		SELECT 
+			PublicationID,
+			Title,
+			FirstName + ' ' + ISNULL(MiddleName, '')  + ' ' + LastName,
+			Topics.Name,
+			Categories.Name, 
+			PublicationHouses.Name,
+			PublicationPlace,
+			ReleaseDate
+		FROM
+			Literature
+			LEFT JOIN Authors ON Authors.AuthorID = Literature.AuthorID
+			LEFT JOIN Topics ON Topics.TopicsID = Literature.TopicID
+			LEFT JOIN Categories ON Categories.CategoriesID = Literature.CategoryID
+			LEFT JOIN PublicationHouses ON PublicationHouses.PublicationHouseID = Literature.PublicationHouseID
+		WHERE
+			Literature.AuthorID = @id
+	RETURN
+END
